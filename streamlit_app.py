b965c6a41a267b8a0536d63e7eb19dff2ea36889
Ravi_Kumar2 <kumar.333@iitj.ac.in>
2023-05-05 00:37:49 +0530

    Trying gpt-2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,62 @@
+# Import necessary libraries
 import streamlit as st
-from transformers import BertTokenizer, BertForSequenceClassification
+import openai
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT3LMHeadModel, GPT3Tokenizer
 
-from train_bert import train
+# Set OpenAI API key for GPT-3
+openai.api_key = "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"
 
-st.title("Social Bias Detection")
-
-# Add a button to trigger the training process
-if st.button("Train Model"):
-    st.write("Training the model. This may take a while...")
-    train()
-    st.write("Model training complete!")
+# Load the GPT-2 and GPT-3 models
+gpt2_model = GPT2LMHeadModel.from_pretrained("gpt2")
+gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 
-# Load the fine-tuned BERT model
-model_path = "trained_model"
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = BertForSequenceClassification.from_pretrained(model_path)
 
-# Function to predict social bias
-def predict_social_bias(text):
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=128)
-    outputs = model(**inputs)
-    logits = outputs.logits
-    probabilities = torch.softmax(logits, dim=1).detach().numpy().tolist()[0]
-    return probabilities
+models = {
+    "GPT-2": gpt2_model,
+}
 
+# Predict hate speech and social bias using the GPT-2 model
+def predict_gpt2(generator, text):
+    # Example implementation using GPT-2 text generation for classification (not accurate)
+    prompt = f"Hate speech classification: {text}"
+    generated_text = generator(prompt, max_length=50, do_sample=True)[0]['generated_text']
+    hate_speech_result = 'yes' in generated_text.lower()
 
-# Streamlit UI
-st.title("Social Bias Detection")
+    prompt = f"Social bias classification: {text}"
+    generated_text = generator(prompt, max_length=50, do_sample=True)[0]['generated_text']
+    social_bias_result = 'yes' in generated_text.lower()
 
-input_text = st.text_area("Enter text to check for social bias:")
-if st.button("Analyze"):
-    if input_text:
-        probabilities = predict_social_bias(input_text)
-        bias_prob = probabilities[1]
-        st.write(f"Probability of social bias: {bias_prob:.2%}")
+    return hate_speech_result, social_bias_result
 
-        if bias_prob > 0.5:
-            st.warning("This text may contain social bias.")
+# Streamlit app
+def main():
+    st.set_page_config(page_title="Hate Speech and Social Bias Detection", layout="wide")
+    st.title("Hate Speech and Social Bias Detection")
+
+    # Model selection
+    model_name = st.sidebar.selectbox("Select a model", list(models.keys()))
+
+    # Input text
+    user_input = st.text_area("Input text for analysis", "")
+
+    # Process input text
+    if st.button("Analyze"):
+        if user_input:
+            model = models[model_name]
+
+            if model_name == "GPT-2":
+                # Hate speech and social bias detection using GPT-2
+                hate_speech_result, social_bias_result = predict_gpt2(gpt2_model, gpt2_tokenizer, user_input)
+
+            # Display results
+            st.write("### Hate Speech Analysis")
+            st.write(f"Hate speech detected: {'Yes' if hate_speech_result else 'No'}")
+
+            st.write("### Social Bias Analysis")
+            st.write(f"Social bias detected: {'Yes' if social_bias_result else 'No'}")
         else:
-            st.success("This text is unlikely to contain social bias.")
-    else:
-        st.warning("Please enter some text to analyze.")
+            st.error("Please provide input text for analysis.")
+
+if __name__ == "__main__":
+    main()
